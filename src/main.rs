c43daa501ddfc69234434c1bc6855034172be053
Jonas Schmidt <jon-schmidt@outlook.de>
2025-11-24 11:13:03 +0100

    update with error

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,15 @@
 use clap::Parser;
-use futures::{Stream, StreamExt, executor::LocalPool, task::LocalSpawnExt};
+use futures::{StreamExt, executor::LocalPool, task::LocalSpawnExt};
+
+#[allow(unused_imports)]
 use log::{debug, info};
-use r2r::{Node, QosProfile, log_info};
+use r2r::QosProfile;
 use serde::{Deserialize, Serialize};
 use std::{
-    convert::Infallible,
     f64::consts::PI,
     net::{Ipv4Addr, SocketAddrV4},
 };
-use tokio::{
-    sync::OnceCell,
-    time::{self, Duration},
-};
+use tokio::sync::OnceCell;
 use warp::Filter;
 
 static CLI_ARGS: OnceCell<CliArgs> = OnceCell::const_new();
@@ -33,58 +31,6 @@ struct EventStructData {
     level: String,
 }
 
-// Function to create a stream of events
-async fn event_stream() -> Result<impl warp::Reply, Infallible> {
-    // let (tx, mut rx) = tokio::sync::mpsc::channel::<EventStructData>(32);
-
-    let ctx = r2r::Context::create().unwrap();
-    let mut node = r2r::Node::create(ctx.clone(), "web_server", "").unwrap();
-    LOGGER_NAME.set(node.logger().to_string()).unwrap();
-
-    let mut subscription = node
-        .subscribe::<r2r::sensor_msgs::msg::Imu>("/imu/imu", QosProfile::default())
-        .unwrap();
-
-    r2r::log_info!(node.logger(), "Intialized");
-
-    info!("Created Subscriber");
-    let args = CLI_ARGS.get().unwrap();
-    info!("Starting with Config: {:#?}", args);
-
-    node.spin_once(Duration::from_secs(1));
-
-    let stream = subscription.map(|msg| {
-        let args = CLI_ARGS.get().unwrap();
-        let level = msg.orientation.y;
-        let level_deg = level / 180.0 * PI;
-        let data = EventStructData {
-            level: format!("Pitch = {}°", (args.target_angle - level_deg)),
-        };
-        let event = warp::sse::Event::default().json_data(data);
-        event
-    });
-
-    // Create a stream of SSE
-    // let stream = async_stream::stream! {
-    //     info!("Starting loop");
-    //     // let node = node;
-    //     let mut subscription = subscription;
-    //     while ctx.is_valid() {
-    //         while let Some(msg) = subscription.next().await {
-    //             let args = CLI_ARGS.get().unwrap();
-    //             let level = msg.orientation.y;
-    //             let level_deg = level / 180.0 * PI;
-    //             let data = EventStructData {level: format!("Pitch = {}°", (args.target_angle - level_deg))};
-    //             let event = warp::sse::Event::default().json_data(data);
-    //             yield event;
-    //         }
-    //         tokio::time::sleep(Duration::from_millis(50)).await;
-    //     }
-    // info!("Stopping loop");
-    // };
-    Ok(warp::sse::reply(warp::sse::keep_alive().stream(stream)))
-}
-
 #[tokio::main]
 async fn main() {
     colog::init();
@@ -99,23 +45,49 @@ async fn main() {
         .allow_methods(vec!["GET"]) // Allow GET requests
         .allow_headers(vec!["Content-Type"]); // Allow 
 
-    // Define a route for SSE
-    let route = warp::path("events")
-        .and(warp::get())
-        .and_then(event_stream)
-        .with(cors);
-
     let mut pool = LocalPool::new();
     let spawner = pool.spawner();
 
-    spawner
-        .spawn_local(async move {
-            // Start the warp server
-            warp::serve(route)
-                .run(CLI_ARGS.get().unwrap().socket) // Listen on localhost:3030
-                .await;
-        })
-        .unwrap();
+    let sse_route = warp::path("events").and(warp::get()).map(|| {
+        let ctx = r2r::Context::create().unwrap();
+        let mut node = r2r::Node::create(ctx.clone(), "web_server", "").unwrap();
+        LOGGER_NAME.set(node.logger().to_string()).unwrap();
 
-    spawner.spawn_local(async move {}).unwrap();
+        let mut subscription = node
+            .subscribe::<r2r::sensor_msgs::msg::Imu>("/imu/imu", QosProfile::default())
+            .unwrap();
+
+        r2r::log_info!(node.logger(), "Intialized");
+
+        let (tx, mut rx) = tokio::sync::mpsc::channel(1);
+        spawner
+            .spawn_local(async move {
+                while let Some(msg) = subscription.next().await {
+                    let angle = msg.orientation.z;
+                    let angle_deg = angle / 180.0 * PI;
+                    let data = EventStructData {
+                        level: format!("Pitch = {}°", (args.target_angle - angle_deg)),
+                    };
+                    let event = warp::sse::Event::default().json_data(data);
+
+                    tx.send(event).await.unwrap();
+                }
+            })
+            .unwrap();
+
+        // Convert receiver to a stream
+        let stream = async_stream::stream! {
+            while let Some(msg) = rx.recv().await {
+                yield msg; // Yield messages to the stream
+            }
+        };
+        warp::sse::reply(warp::sse::keep_alive().stream(stream))
+    });
+
+    let route = sse_route.with(cors);
+
+    // Start the warp server
+    warp::serve(route)
+        .run(CLI_ARGS.get().unwrap().socket) // Listen on localhost:3030
+        .await;
 }
